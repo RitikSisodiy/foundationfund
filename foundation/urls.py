3fc125aa435bee72b44393d1762d5a835320fbc9
ritik sisiodiya <ritik.s10120@gmail.com>
2021-11-15 17:34:00 +0530

    install all required apps

--- foundation/urls.py
+++ foundation/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
@@ -25,4 +26,4 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('gallary/', include('gallary.urls')),
     path('shop/', include('shop.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
