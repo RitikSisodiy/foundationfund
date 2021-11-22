1df7ddeea88d774bb01d9a36497585870dac5637
Your Name <ritik.s10120.com>
2021-11-22 01:15:07 -0700

    some changes

--- about/models.py
+++ about/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.enums import Choices
 
 # Create your models here.
+class VolunteerRegister(models.Model):
+    choices = (
+        ( 'Applied','Applied' ),
+        ( 'Reviewed','Reviewed' ),
+        ( 'Pending','Pending' ),
+        ( 'Verified','Verified' ),
+    )
+    name = models.CharField(max_length=40)
+    email = models.CharField(max_length=40)
+    gender = models.CharField(max_length=10)
+    branch = models.CharField(max_length=40)
+    message = models.CharField(max_length=40)
+    resume = models.FileField(upload_to='resume')
+    status = models.CharField(max_length=20,choices=choices,default="Applied")
--- about/views.py
+++ about/views.py
@@ -1,12 +1,24 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views.decorators import csrf
 from django.views.decorators.csrf import csrf_exempt
+from superuser.forms import GenForm
+from .models import VolunteerRegister
 # Create your views here.
 def about(request):
     return render(request,'about/about.html')
 @csrf_exempt
 def volunteer(request):
     if request.method == "POST":
+        resjson = {}
+        form = GenForm(VolunteerRegister)
+        form = form(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            resjson["message"] = "Thanks For Registration We Will Get Back To You Soon :)"
+            print("success")
+            return JsonResponse(resjson)
+        print(request.POST,request.FILES)
         pass
     return render(request,'about/volunteer.html')
 
